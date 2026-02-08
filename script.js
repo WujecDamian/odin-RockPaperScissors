f2c5b12308215a520ef82f238d2d6bc7548ad25f
WujecDamian <wujecdamian12@gmail.com>
2026-02-08 14:30:10 +0100

    feat: Add human choice input for Rock Paper Scissors (RPS)

--- script.js
+++ script.js
@@ -15,6 +15,17 @@ function getComputerChoice () {
   }
 }
 function getHumanChoice () {
-  let humanInput = prompt()
+  let humanInput = prompt('1-Rock, 2-Paper, 3-Scissors')
+  switch (humanInput) {
+    case 1:
+      return 'rock'
+      break
+    case 2:
+      return 'paper'
+      break
+    case 3:
+      return 'scissors'
+      break
+  }
 }
 console.log(getComputerChoice())
