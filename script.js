c72fab0621df5747a0ad848ce8f5adad72b69e5f
WujecDamian <wujecdamian12@gmail.com>
2026-02-08 14:29:00 +0100

    feat: Create function with random computer choice (RPS)

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log('helu wort!')
+
+function getComputerChoice () {
+  let computerOutput = Math.floor(Math.random() * 3) + 1
+  switch (computerOutput) {
+    case 1:
+      return 'rock'
+      break
+    case 2:
+      return 'paper'
+      break
+    case 3:
+      return 'scissors'
+      break
+  }
+}
+function getHumanChoice () {
+  let humanInput = prompt()
+}
+console.log(getComputerChoice())
